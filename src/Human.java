0e5860240a750add6d569b80ac0a3eca9a1aae25
krela <krel.andrej@yandex.ru>
2022-11-07 20:53:10 +0700

    asddsad

--- src/Human.java
+++ src/Human.java
@@ -6,10 +6,18 @@ public class Human {
 
 
     public Human(int yearOfBrith, String name, String town, String post){
-        this.yearOfBrith = yearOfBrith;
-        this.name = name;
-        this.town = town;
-        this.post = post;
+        if(yearOfBrith<0){
+            this.yearOfBrith =0;
+        }else{this.yearOfBrith = yearOfBrith;}
+        if (name == null || name == "") {
+            this.name = "Информация не указана ";
+        }else{this.name = name;}
+        if(town == null || town == ""){
+            this.town = "Информация не указана ";
+        }else{this.town = town;}
+        if(post == null||post ==""){
+            this.post = "Информация не указана ";
+        }else{this.post = post;}
     }
 
     public int getYearOfBrith() {
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
         Human anna = new Human(1993,"Аня","Москва","методист образовательных программ");
         Human kata = new Human(1992,"Катя","Калининград","продакт менеджер");
         Human artem = new Human(1995,"Артем","Москва","директор по развитию бизнеса ");
+        Human pavel = new Human(-1,null,"","");
         System.out.println(maxim.toString());
         System.out.println(anna.toString());
         System.out.println(kata.toString());
         System.out.println(artem.toString());
+        System.out.println(pavel.toString());
     }
 }
